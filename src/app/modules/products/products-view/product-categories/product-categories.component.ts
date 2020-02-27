b1878fbdf044b2972e58101bd735e2da622eef2a
Mihai <mihai.ioan98@gmail.com>
2020-02-27 18:08:51 +0200

    Add product-categories component and adjust position in page

---
+++ src/app/modules/products/products-view/product-categories/product-categories.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-categories',
+  templateUrl: './product-categories.component.html',
+  styleUrls: ['./product-categories.component.css']
+})
+export class ProductCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clickHandler(event: any) {
+    console.log(event.target.innerText);
+  }
+
+}
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -5,11 +5,12 @@ import { ProductsViewComponent } from './products-view/products-view.component';
 import { ProductComponent } from './products-view/product/product.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { ProductCategoriesComponent } from './products-view/product-categories/product-categories.component';
 
 
 
 @NgModule({
-  declarations: [ProductsViewComponent, ProductComponent],
+  declarations: [ProductsViewComponent, ProductComponent, ProductCategoriesComponent],
   exports: [
     ProductsViewComponent
   ],
